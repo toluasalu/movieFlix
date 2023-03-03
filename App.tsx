c8314b0cb92444b1ff5586d25cd9370c263a989e
toluasalu <johnasalu13@gmail.com>
2023-03-03 15:53:35 +0100

    first version of app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { SafeAreaView, StyleSheet, ActivityIndicator,  View} from 'react-native';
+//import Card
+import { Card, Avatar, Button, Text } from 'react-native-paper';
+import AvatarComponent from './Avatar';
+
+
+
+
+
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies =async () => {
+     try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies)
+     } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+     useEffect(() => {
+        getMovies();
+     }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style= {styles.container}>
+     <View style={styles.container}>
+    
+      {}
+        
       <StatusBar style="auto" />
-    </View>
+     </View>
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +50,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20
   },
 });
---
+++ Avatar.tsx
@@ -0,0 +1,8 @@
+//Import avatar
+import { Avatar} from 'react-native-paper';
+
+const AvatarComponent = () =>(
+    <Avatar.Image size={48} source ={require('../assets/avatar.png')}/>
+)
+
+export default AvatarComponent;
